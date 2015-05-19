13f8f6d9e6477d20c450ea3646a22d1084b44db0
Taku <Taku@192.168.1.82>
2015-05-19 16:11:04 +0200

    Botones de Login y Registro dentro.

--- GamesDB/src/vista/VistaApp.java
+++ GamesDB/src/vista/VistaApp.java
@@ -48,19 +48,34 @@ public class VistaApp extends JFrame {
 			//BARRA DEL MENU
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);	
+		
+		JButton btnMenuPrincipal = new JButton("Principal");
+		btnMenuPrincipal.setContentAreaFilled(false);
+		btnMenuPrincipal.setBorderPainted(false);
+		menuBar.add(btnMenuPrincipal);
+		
+		JButton btnMenuJuegos = new JButton("Juegos");
+		btnMenuJuegos.setContentAreaFilled(false);
+		btnMenuJuegos.setBorderPainted(false);
+		menuBar.add(btnMenuJuegos);
+		
+		JButton btnMenuPerfil = new JButton("Perfil");
+		btnMenuPerfil.setContentAreaFilled(false);
+		btnMenuPerfil.setBorderPainted(false);
+		menuBar.add(btnMenuPerfil);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 			//BOTONES
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(265, 115, 90, 25);
-		contentPane.add(btnNewButton);
+		JButton btnJuegos = new JButton("Juegos");
+		btnJuegos.setBounds(265, 115, 90, 25);
+		contentPane.add(btnJuegos);
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(265, 150, 90, 25);
-		contentPane.add(btnNewButton_1);
+		JButton btnPerfil = new JButton("Perfil");
+		btnPerfil.setBounds(265, 150, 90, 25);
+		contentPane.add(btnPerfil);
 	
 			//INPUT TEXTO
 		nomUsuario = new JTextField();
@@ -73,6 +88,15 @@ public class VistaApp extends JFrame {
 		contentPane.add(passUsuario);
 		passUsuario.setColumns(10);
 		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(15, 340, 175, 20);
+		btnLogin.setContentAreaFilled(false);
+		contentPane.add(btnLogin);
+		
+		JButton btnReg = new JButton("Registrarse");
+		btnReg.setBounds(15, 370, 175, 20);
+		btnReg.setContentAreaFilled(false);
+		contentPane.add(btnReg);
 		
 			//IMAGENES
 		Image niggerCobra = new ImageIcon(this.getClass().getResource("/NiggerCobra.jpg")).getImage();
@@ -81,25 +105,27 @@ public class VistaApp extends JFrame {
 		lblImage.setIcon(new ImageIcon(niggerCobra));
 		getContentPane().add(lblImage);
 		
-		Image triForce = new ImageIcon(this.getClass().getResource("/Triforce.png")).getImage();
-		JLabel lblFondo = new JLabel();
-		lblFondo.setBounds(75, 0, 425, 475);
-		lblFondo.setIcon(new ImageIcon(triForce));
-		this.add(lblFondo);
+	
 			//--------------------------------------------//
 		
 			//TEXTO
-		lblUser = new JLabel("Usuario :");
+		lblUser = new JLabel("Usuario : ");
 		lblUser.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUser.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblUser.setBounds(15, 210, 85, 40);
 		contentPane.add(lblUser);
 		
-		lblPass = new JLabel("Contrase\u00F1a :");
+		lblPass = new JLabel("Contrase\u00F1a : ");
 		lblPass.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPass.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblPass.setBounds(15, 265, 110, 40);
 		contentPane.add(lblPass);
+		
+		Image triForce = new ImageIcon(this.getClass().getResource("/Triforce.png")).getImage();
+		JLabel lblFondo = new JLabel();
+		lblFondo.setBounds(75, 0, 425, 475);
+		lblFondo.setIcon(new ImageIcon(triForce));
+		getContentPane().add(lblFondo);
 		
 	
 		
